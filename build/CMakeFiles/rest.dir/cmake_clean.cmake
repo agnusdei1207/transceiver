41a423bf9a228983542251dfbb088c3e0c8b5062
psw <agnusdei1207@gmail.com>
2025-01-19 23:33:13 +0900

    chore(update): cmake

---
+++ build/CMakeFiles/rest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rest.dir/main.c.o"
+  "CMakeFiles/rest.dir/main.c.o.d"
+  "rest"
+  "rest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
